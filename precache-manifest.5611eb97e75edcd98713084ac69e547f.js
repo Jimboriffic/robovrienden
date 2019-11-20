896b8d9b6229c382c7777029e2169069630b575b
James <j.sliwa@mail.com>
2019-11-20 21:17:05 +0100

    Updates

---
+++ precache-manifest.5611eb97e75edcd98713084ac69e547f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134c7ff9bd9f1a47bce7275f16cd9f7d",
+    "url": "/robovrienden/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robovrienden/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "200420b08bedae2bc4ce",
+    "url": "/robovrienden/static/css/main.54cee5f5.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robovrienden/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "200420b08bedae2bc4ce",
+    "url": "/robovrienden/static/js/main.47f1e4c1.chunk.js"
+  },
+  {
+    "revision": "b15e4a448e847d20ca3b",
+    "url": "/robovrienden/static/js/runtime-main.6d4731cc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robovrienden/static/media/SEGA.12a3db8b.woff"
+  }
+]);
